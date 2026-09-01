a69423f5d4c47a3830e8b9517baa89e42d6e96f1
gilzoide <gilzoide@gmail.com>
2026-09-01 11:09:51 -0300

    Update tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,5 @@
+#include <exception>
+#include <stdexcept>
 #include <thread>
 
 #include <catch2/catch_test_macros.hpp>
@@ -5,119 +7,158 @@
 
 using namespace std::chrono_literals;
 
-TEST_CASE("Dispatch Queue") {
-	SECTION("Synchronous") {
-		dispatch_queue::dispatch_queue q(0);
-		REQUIRE(!q.is_threaded());
+TEST_CASE("Synchronous") {
+	dispatch_queue::dispatch_queue q(0);
+	REQUIRE(!q.is_threaded());
 
-		auto future = q.dispatch([]{ return 42; });
-		REQUIRE(future.get() == 42);
+	auto future = q.dispatch([]{ return 42; });
+	REQUIRE(future.get() == 42);
 
-		auto thread_id = std::this_thread::get_id();
-		INFO("Test thread ID" << thread_id);
-		q.dispatch([=]{
-			REQUIRE(std::this_thread::get_id() == thread_id);
-		});
+	auto thread_id = std::this_thread::get_id();
+	INFO("Test thread ID" << thread_id);
+	q.dispatch([=]{
+		REQUIRE(std::this_thread::get_id() == thread_id);
+	});
 
-		q.wait();
-	}
-
-	SECTION("Serial") {
-		dispatch_queue::dispatch_queue q(1);
-		REQUIRE(q.is_threaded());
-
-		auto future = q.dispatch([]{ return 42; });
-		REQUIRE(future.get() == 42);
-
-		auto thread_id = std::this_thread::get_id();
-		INFO("Test thread ID" << thread_id);
-		auto task = q.dispatch([=]{
-			REQUIRE(std::this_thread::get_id() != thread_id);
-		});
-
-		task.wait();
-	}
-
-	SECTION("Concurrent") {
-		dispatch_queue::dispatch_queue q(-1);
-		REQUIRE(q.is_threaded());
-
-		auto future = q.dispatch([]{ return 42; });
-		REQUIRE(future.get() == 42);
-
-		auto thread_id = std::this_thread::get_id();
-		INFO("Test thread ID" << thread_id);
-		for (int i = 0; i < 10; i++) {
-			q.dispatch([=]{
-				REQUIRE(std::this_thread::get_id() != thread_id);
-			});
-		}
-
-		q.wait();
-	}
-
-	SECTION("Main loop") {
-		dispatch_queue::dispatch_queue q(-1);
-
-		auto thread_id = std::this_thread::get_id();
-		auto task = q.dispatch_main([=]{
-			REQUIRE(std::this_thread::get_id() == thread_id);
-			return 42;
-		});
-		REQUIRE(task.get_state() == dispatch_queue::task_state::pending);
-		q.main_loop();
-		REQUIRE(task.get_state() == dispatch_queue::task_state::ready);
-		REQUIRE(task.get() == 42);
-	}
-
-	SECTION("Main loop dependency") {
-		dispatch_queue::dispatch_queue q(-1);
-
-		auto thread_id = std::this_thread::get_id();
-		auto task = q.dispatch([=]{
-			return 42;
-		}).then([=, &q](auto t) {
-			// This continuation may or may not run inside a background thread, it depends if `then` is called before or after task is pending execution
-			return q.dispatch_main([=] {
-				REQUIRE(std::this_thread::get_id() == thread_id);
-				REQUIRE(t.get() == 42);
-				return t.get() + 1;
-			});
-		}).then([=](auto t) {
-			return t.get() + 2;
-		});
-
-		while (task.get_state() == dispatch_queue::task_state::pending) {
-			q.main_loop();
-		}
-		REQUIRE(task.get_state() == dispatch_queue::task_state::ready);
-		REQUIRE(task.get() == 42 + 1 + 2);
-	}
-
-#ifdef __cpp_impl_coroutine
-	SECTION("Dispatch awaiters") {
-		dispatch_queue::dispatch_queue q(-1);
-
-		auto thread_id = std::this_thread::get_id();
-		auto coro = [&, thread_id]() -> dispatch_queue::task<int> {
-			REQUIRE(std::this_thread::get_id() == thread_id);
-			co_await q.dispatch();
-			REQUIRE(std::this_thread::get_id() != thread_id);
-			co_await q.dispatch_main();
-			REQUIRE(std::this_thread::get_id() == thread_id);
-			int value = co_await q.dispatch([]{ return 3; });
-			REQUIRE(value == 3);
-			co_return value;
-		}();
-		while (coro.get_state() != dispatch_queue::task_state::ready) {
-			q.main_loop();
-		}
-		REQUIRE(coro.get() == 3);
-	}
-#endif
+	q.wait();
 }
 
-TEST_CASE("Parallel for each") {
+TEST_CASE("Serial") {
+	dispatch_queue::dispatch_queue q(1);
+	REQUIRE(q.is_threaded());
+
+	auto future = q.dispatch([]{ return 42; });
+	REQUIRE(future.get() == 42);
+
+	auto thread_id = std::this_thread::get_id();
+	INFO("Test thread ID" << thread_id);
+	auto task = q.dispatch([=]{
+		REQUIRE(std::this_thread::get_id() != thread_id);
+	});
+
+	task.wait();
+}
+
+TEST_CASE("Concurrent") {
+	dispatch_queue::dispatch_queue q(-1);
+	REQUIRE(q.is_threaded());
+
+	auto future = q.dispatch([]{ return 42; });
+	REQUIRE(future.get() == 42);
+
+	auto thread_id = std::this_thread::get_id();
+	INFO("Test thread ID" << thread_id);
+	for (int i = 0; i < 10; i++) {
+		q.dispatch([=]{
+			REQUIRE(std::this_thread::get_id() != thread_id);
+		});
+	}
+
+	q.wait();
+}
+
+TEST_CASE("Main loop") {
+	dispatch_queue::dispatch_queue q(-1);
+
+	auto thread_id = std::this_thread::get_id();
+	auto task = q.dispatch_main([=]{
+		REQUIRE(std::this_thread::get_id() == thread_id);
+		return 42;
+	});
+	REQUIRE(task.get_state() == dispatch_queue::task_state::pending);
+	q.main_loop();
+	REQUIRE(task.get_state() == dispatch_queue::task_state::ready);
+	REQUIRE(task.get() == 42);
+}
+
+TEST_CASE("Main loop dependency") {
+	dispatch_queue::dispatch_queue q(-1);
+
+	auto thread_id = std::this_thread::get_id();
+	auto task = q.dispatch([=]{
+		return 42;
+	}).then([=, &q](auto t) {
+		// This continuation may or may not run inside a background thread, it depends if `then` is called before or after task is pending execution
+		return q.dispatch_main([=] {
+			REQUIRE(std::this_thread::get_id() == thread_id);
+			REQUIRE(t.get() == 42);
+			return t.get() + 1;
+		});
+	}).then([=](auto t) {
+		return t.get() + 2;
+	});
+
+	while (task.get_state() == dispatch_queue::task_state::pending) {
+		q.main_loop();
+	}
+	REQUIRE(task.get_state() == dispatch_queue::task_state::ready);
+	REQUIRE(task.get() == 42 + 1 + 2);
+}
+
+#ifdef __cpp_impl_coroutine
+TEST_CASE("Dispatch awaiters") {
+	dispatch_queue::dispatch_queue q(-1);
+
+	auto thread_id = std::this_thread::get_id();
+	auto coro = [&, thread_id]() -> dispatch_queue::task<int> {
+		REQUIRE(std::this_thread::get_id() == thread_id);
+		co_await q.dispatch();
+		REQUIRE(std::this_thread::get_id() != thread_id);
+		co_await q.dispatch_main();
+		REQUIRE(std::this_thread::get_id() == thread_id);
+		int value = co_await q.dispatch([]{ return 3; });
+		REQUIRE(value == 3);
+		co_return value;
+	}();
+	while (coro.get_state() != dispatch_queue::task_state::ready) {
+		q.main_loop();
+	}
+	REQUIRE(coro.get() == 3);
+}
+#endif
+
+TEST_CASE("when_all") {
+	SECTION("all success") {
+		auto task = dispatch_queue::when_all(
+			dispatch_queue::task<>::create_ready(),
+			dispatch_queue::task<>::create_ready(),
+			dispatch_queue::task<>::create_ready()
+		);
+		REQUIRE(task.get_state() == dispatch_queue::task_state::ready);
+	}
+
+	SECTION("all failed") {
+		auto task = dispatch_queue::when_all(
+			dispatch_queue::task<>::create_failed(std::make_exception_ptr(std::runtime_error(""))),
+			dispatch_queue::task<>::create_failed(std::make_exception_ptr(std::runtime_error(""))),
+			dispatch_queue::task<>::create_failed(std::make_exception_ptr(std::runtime_error("")))
+		);
+		REQUIRE(task.get_state() == dispatch_queue::task_state::failed);
+	}
+
+	SECTION("one failed") {
+		auto task = dispatch_queue::when_all(
+			dispatch_queue::task<>::create_ready(),
+			dispatch_queue::task<>::create_ready(),
+			dispatch_queue::task<>::create_failed(std::make_exception_ptr(std::runtime_error("")))
+		);
+		REQUIRE(task.get_state() == dispatch_queue::task_state::failed);
+	}
+
+	SECTION("one pending") {
+		auto future = dispatch_queue::detail::task_future<void>::create_pending();
+		auto task = dispatch_queue::when_all(
+			dispatch_queue::task<>::create_ready(),
+			dispatch_queue::task<>::create_ready(),
+			dispatch_queue::task(future)
+		);
+		REQUIRE(task.get_state() == dispatch_queue::task_state::pending);
+		future->set_value();
+		REQUIRE(task.get_state() == dispatch_queue::task_state::ready);
+	}
+}
+
+TEST_CASE("parallel_for") {
 	bool finished[1024] = {};
 	for (bool b : finished) {
 		REQUIRE(!b);
